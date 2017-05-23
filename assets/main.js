e27c337c6d9c0aacdac206a379e0c87dc685548b
Jose Uscanga <jousmo@outlook.com>
2017-05-23 14:29:13 -0500

    Complete

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,37 @@
-$(function() {
+/* global $ */
 
-  // your code will go here
+$(function () {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/engel_zamudio.json',
+    dataType: 'jsonp',
+    success: function (data) {
+      populateWithCourses(data.courses.completed)
+    }
+  })
 
-});
+  function populateWithCourses (courses) {
+    var $badges = $('#badges')
+    var $div
+
+    courses.forEach(function (course) {
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges)
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div)
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div)
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div)
+    })
+  }
+})
